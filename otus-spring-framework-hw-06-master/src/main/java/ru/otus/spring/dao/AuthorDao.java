ee6dc807e2352c65956c00d75c388a9f38c0f750
Amrudin Abdurizaev <77205579+Amru05@users.noreply.github.com>
2023-02-20 14:15:55 +0300

    Delete otus-spring-framework-hw-06-master directory

--- otus-spring-framework-hw-06-master/src/main/java/ru/otus/spring/dao/AuthorDao.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.spring.dao;
-
-import ru.otus.spring.domain.Author;
-
-public interface AuthorDao extends Dao<Author> {
-	
-}
--- otus-spring-framework-hw-06-master/src/main/java/ru/otus/spring/dao/GenreDao.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.spring.dao;
-
-import ru.otus.spring.domain.Genre;
-
-public interface GenreDao extends Dao<Genre> {
-	
-}
